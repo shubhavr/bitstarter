6ddfa4cca6cd798ef8374fce9f5572a1c510804a
Shubha Raghavan <shubha.raghavan@gmail.com>
2013-07-12 03:13:11 +0000

    edit for part1 assignment

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('/home/ubuntu/hw3/bitstarter/index.html', 
+      function (err, data) {
+    if (err) throw err;
+    //console.log(data);
+    response.send(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
