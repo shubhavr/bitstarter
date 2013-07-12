2b002dd83159afd56c42e086e3dcaa00ad9e74bd
Shubha Raghavan <shubha.raghavan@gmail.com>
2013-07-12 03:15:52 +0000

    edit 2

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 
 app.get('/', function(request, response) {
   fs.readFile('/home/ubuntu/hw3/bitstarter/index.html', 
